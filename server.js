29301ef68fce7fcda5acf3f6a594f8f2879cb30b
lin <lin.ferr@mail.ru>
2026-06-16 09:15:23 +0500

    workwork

--- server.js
+++ server.js
@@ -345,7 +345,7 @@ async function startServer() {
         const distPath = path.join(__dirname, 'dist');
         if (fs.existsSync(distPath)) {
             app.use(express.static(distPath));
-            app.get('*', (req, res) => {
+            app.get('/*', (req, res) => {
                 if (!req.path.startsWith('/api')) {
                     res.sendFile(path.join(distPath, 'index.html'));
                 }
